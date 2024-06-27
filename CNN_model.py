fb953d4df7b2e7f88f87139f8b6cd9e439c55c85
Eduardo Ruiz <154444699+1408eduardo@users.noreply.github.com>
2024-06-27 13:01:56 -0600

    Create CNN_model.py

---
+++ CNN_model.py
@@ -0,0 +1,17 @@
+# cnn_model.py
+
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+def create_cnn_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=CNN_CONSTANTS['IMAGE_SIZE'] + (3,)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(CNN_CONSTANTS['NUM_CLASSES'], activation='softmax'))
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
